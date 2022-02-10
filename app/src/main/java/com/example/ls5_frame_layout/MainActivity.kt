6f95268c791357a79f5c67d0ae456fea0daecd75
Rion McLaren <ramclaren@quinnipiac.edu>
2022-02-10 13:32:53 -0500

    Initial commit

---
+++ app/src/main/java/com/example/ls5_frame_layout/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.ls5_frame_layout
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
